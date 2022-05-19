948d8a5c98249e6aec9c76b541b84a5cc0638fa7
Lucas <lucas.fn4@gmail.com>
2022-05-19 15:21:08 -0300

    adiciona elementos no menu principal/tela de jogo

--- game.py
+++ game.py
@@ -10,37 +10,64 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.running = True
-        self.state = 'start'
+        self.state = 'menu'
 
     def run_game(self):
         while self.running:
-            if self.state == 'start':
-                self.start_events()
-                self.start_update()
-                self.start_draw()
+            if self.state == 'menu':
+                self.menu_events()
+                self.menu_update()
+                self.menu_draw()
+            elif self.state == 'playing':
+                self.playing_events()
+                self.playing_update()
+                self.playing_draw()
+            else:
+                self.running = False
+
             self.clock.tick(FPS)
         pygame.quit()
         sys.exit()
 
-    def draw_text(self, string, screen, text_position, size, colour, font_name):
+    def draw_text(self, string, screen, text_position, size, colour, font_name,isCentered = False):
         font = pygame.font.SysFont(font_name, size)
         text = font.render(string, False, colour)
         text_size = text.get_size()
-        text_position[0] = text_position[0] - text_size[0]/2
-        text_position[1] = text_position[1] - text_size[1]/2
+        if isCentered:
+            text_position[0] = text_position[0] - text_size[0]/2
+            text_position[1] = text_position[1] - text_size[1]/2
+
         screen.blit(text, text_position)
 
-    def start_events(self):
+    def menu_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 self.state = 'playing'
     
-    def start_update(self):
+    def menu_update(self):
         pass
 
-    def start_draw(self):
+    def menu_draw(self):
         self.screen.fill(COLOR_BLACK)
-        self.draw_text('PUSH SPACE BAR', self.screen, [SCREEN_WIDHT//2, SCREEN_HEIGHT//2], START_TEXT_SIZE, COLOR_ORANGE, START_FONT)
+        self.draw_text('PUSH SPACE BAR', self.screen, [SCREEN_WIDHT//2, SCREEN_HEIGHT//2], MENU_TEXT_SIZE+5, COLOR_ORANGE, MENU_FONT, True)
+        self.draw_text('ONE PLAYER ONLY', self.screen, [SCREEN_WIDHT//2, SCREEN_HEIGHT//2 + 50], MENU_TEXT_SIZE, COLOR_BLUE, MENU_FONT,True)
+        self.draw_text('HIGH SCORE', self.screen, [4,0], MENU_TEXT_SIZE, COLOR_WHITE, MENU_FONT)
         pygame.display.update()
+
+######### jogo #########
+    def playing_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            
+    
+    def playing_update(self):
+        pass
+
+    def playing_draw(self):
+        self.screen.fill(COLOR_ORANGE) 
+        pygame.display.update()
+
+    
