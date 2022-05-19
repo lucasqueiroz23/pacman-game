b6ef2425a2d7d3242b4ec3abe5a360fba0fa6eee
Manshyei <caiocesar31951910@hotmail.com>
2022-05-19 14:36:17 -0300

    Adicionar configuracoes do Menu

--- game.py
+++ game.py
@@ -10,26 +10,37 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.running = True
-        self.state = 'intro'
+        self.state = 'start'
 
     def run_game(self):
         while self.running:
-            if self.state == 'intro':
-                self.intro_events()
-                self.intro_update()
-                self.intro_draw()
-
+            if self.state == 'start':
+                self.start_events()
+                self.start_update()
+                self.start_draw()
+            self.clock.tick(FPS)
         pygame.quit()
         sys.exit()
 
-    def intro_events(self):
+    def draw_text(self, string, screen, text_position, size, colour, font_name):
+        font = pygame.font.SysFont(font_name, size)
+        text = font.render(string, False, colour)
+        text_size = text.get_size()
+        text_position[0] = text_position[0] - text_size[0]/2
+        text_position[1] = text_position[1] - text_size[1]/2
+        screen.blit(text, text_position)
+
+    def start_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.state = 'playing'
     
-    def intro_update(self):
+    def start_update(self):
         pass
 
-    def intro_draw(self):
-        self.screen.fill((255, 255, 255))
+    def start_draw(self):
+        self.screen.fill(COLOR_BLACK)
+        self.draw_text('PUSH SPACE BAR', self.screen, [SCREEN_WIDHT//2, SCREEN_HEIGHT//2], START_TEXT_SIZE, COLOR_ORANGE, START_FONT)
         pygame.display.update()
--- main.py
+++ main.py
@@ -3,4 +3,3 @@ from game import *
 if __name__ == '__main__':
     game = Game()
     game.run_game()
-
